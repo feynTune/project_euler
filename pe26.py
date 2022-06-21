c96b68d3e7eff32d9142610f8371d3f9459fba0f
feynData <stumpand@gmail.com>
2022-06-21 16:16:41 -0400

    Bigger than float precision

--- pe26.py
+++ pe26.py
@@ -1,17 +1,21 @@
-import numpy as np
+# project euler problem #26. Cannot use float precision
+# for 1-1000 because max repeat length is d-1. This also
+# means we can count down and stop when repeat length > d-1
+# We don't need the actual fraction, but the length of repeats.
+# That means we can analyze when remainders repeat
+
+# initialize length of longest repeating sequence: repeat_length
+repeat_length = 0
 
 #loop over all possible denominators
-for denom in range(10, 1001)
-    decimal = 1/float(denom)
-    decimal_str = str(decimal)
-    print('denom: ', str(denom), ', decimal: ', str(decimal_str))
+for denom in range(1001, 1, -1)
+    if (repeat_length > denom) break
+    
+    #list of digits found for this denom: digits
+    digits = []
+    x = 1
+    index = 0
 
-    #initialize digits to check and strip
-    repeat_num = ''
-    max_len = 0
-    decimal_str.strip('0.')
-
-    #loop over different digits in the number and check for repeats (use in keyword)
-    for digit in decimal_str:
-        repeat_num.append(digit)
-        if(repeat_num in )
+    while(x!=0):
+        remainder = x%denom
+        digits
